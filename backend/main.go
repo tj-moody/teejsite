2369a9d65322b4b77ead86aea914f3a60d0e87f1
tj-moody <92702993+tj-moody@users.noreply.github.com>
2025-05-13 22:06:40 -0400

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Video struct {
+	Title        string `json:"title"`
+	ThumbnailURL string `json:"thumbnail_url"`
+	VideoURL     string `json:"video_url"`
+	Uploader     string `json:"uploader"`
+}
+
+var VIDEOS = []Video{
+	{
+		Title:        "Classic - MKTO",
+		ThumbnailURL: "https://placehold.co/600x400",
+		VideoURL:     "https://www.youtube.com/watch?v=4Ba_qTPA4Ds",
+		Uploader:     "user1",
+	},
+	{
+		Title:        "Let Down - Radiohead",
+		ThumbnailURL: "https://placehold.co/600x400",
+		VideoURL:     "https://www.youtube.com/watch?v=ZVgHPSyEIqk",
+		Uploader:     "user2",
+	},
+}
+
+func enableCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*") // or specific domain for security
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func HandleVideos(w http.ResponseWriter, r *http.Request) {
+	enableCORS(w)
+	videos := VIDEOS
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(videos)
+	log.Println("Request", r.Method, r.URL.Path, r.UserAgent())
+}
+
+func main() {
+	http.HandleFunc("/api/goodtube", HandleVideos)
+	log.Println("Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ frontend/goodtube/src/index.js
@@ -0,0 +1,33 @@
+"use strict";
+const API_BASE = "http://localhost:8080";
+async function import_videos() {
+    // Video schema:
+    // `title`: string
+    // `thumbnail_url`: string
+    // `video_url`: string
+    // `uploader`: string
+    const result = await fetch(`${API_BASE}/api/goodtube`);
+    const videos = await result.json();
+    const container = document.getElementById("videos");
+    if (container === null) {
+        return;
+    }
+    container.innerHTML = "";
+    for (const video of videos) {
+        const node = document.createElement("a");
+        node.href = video.video_url;
+        node.target = "_blank"; // Open in new tab
+        node.innerHTML = `<img src="${video.thumbnail_url}" alt="${video.title}"/>\n`;
+        node.id = "video-card";
+        const title = document.createElement("h2");
+        title.innerText = video.title;
+        title.id = "video-title";
+        const info = document.createElement("h3");
+        info.innerText = video.uploader;
+        info.id = "video-info";
+        container.appendChild(node);
+        container.appendChild(title);
+        container.appendChild(info);
+    }
+}
+import_videos().catch(console.error);
