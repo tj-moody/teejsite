f577d2321b5e0c9643c14dbfc67ecda84b045f0d
tj-moody <92702993+tj-moody@users.noreply.github.com>
2026-09-23 08:37:54 -0400

    add resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home";
 import Program from "./pages/Program/program";
+import Resume from "./pages/Resume/resume";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/program"  element={<Program />} />
+                <Route path="/resume"  element={<Resume />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/Resume/resume.tsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+        <div style={{ height: "100vh" }}><iframe
+            src="/resume_9-14-2026.pdf"
+            width="100%"
+            height="100%"
+            title="PDF Viewer"
+        /></div>
+    );
+}
